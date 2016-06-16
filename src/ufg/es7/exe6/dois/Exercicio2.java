693667b45a38eef0a64dd9735156b3d99427f009
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-06-16 19:26:59 -0300

    Adicionando exercício 2.

--- src/ufg/es7/exe6/dois/Exercicio2.java
+++ src/ufg/es7/exe6/dois/Exercicio2.java
@@ -1,18 +1,51 @@
 package ufg.es7.exe6.dois;
 
-import ufg.es7.exe6.um.Exercicio1;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exercicio2 extends Thread {
 
+    public int[][] matriz = new int[50][50];
+    public int coluna;
+
+    public Exercicio2() {
+    }
+
+    public Exercicio2(int[][] matriz, int coluna) {
+        this.matriz = matriz;
+        this.coluna = coluna;
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
-            Thread executor = new Thread(new Exercicio1());
-            executor.start();
+        Exercicio2 exe2 = new Exercicio2();
+        for (int i = 0; i < 50; i++) {
+            for (int j = 0; j < 50; j++) {
+                exe2.matriz[i][j] = i + j;
+            }
         }
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            Exercicio2 umaThread = new Exercicio2(exe2.matriz, i);
+            umaThread.start();
+            threads.add(umaThread);
+        }
+        for (int i = 0; i < 50; i++) {
+            int soma = 0;
+            for (int j = 0; j < 50; j++) {
+                soma += exe2.matriz[i][j];
+            }
+            System.out.println("Soma da linha " + i + " é " + soma);
+        }
+
     }
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        int somaColuna = 0;
+        for (int j = 0; j < 50; j++) {
+            somaColuna += this.matriz[j][this.coluna];
+        }
+        System.out.println("Soma da coluna " + this.coluna + " é " + somaColuna);
     }
+
 }
